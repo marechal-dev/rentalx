88df0e600796b24b45b29a58878ebfc3f9de3eb4
Pietro Piva Vieira <pietro.developer@gmail.com>
2022-10-07 10:48:30 -0300

    Renormalizing EOL

---
+++ src/modules/cars/useCases/editCategory/EditCategoryUseCase.ts
@@ -0,0 +1,9 @@
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+
+class EditCategoryUseCase {
+	constructor(private categoryRepository: CategoriesRepository) {}
+
+	handle(request: Request, response: Response): Response {
+		const  = response.body;
+	}
+}
