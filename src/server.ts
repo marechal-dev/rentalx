28ddd252013bf5854b9e194745881451e68da5ba
Pietro Piva Vieira <pietro.developer@gmail.com>
2022-11-17 21:25:16 -0300

    More shit

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { config as useDotEnv } from "dotenv";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 import { serve, setup } from "swagger-ui-express";
 
 import { createConnection } from "./database/data-source";
 import "./shared/container";
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -21,6 +23,19 @@ app.use(express.json());
 app.use("/api-docs", serve, setup(swaggerFile));
 
 app.use(router);
+
+app.use(
+	(err: Error, request: Request, response: Response, next: NextFunction) => {
+		if (err instanceof AppError) {
+			return response.status(err.statusCode).json({ error: err.message });
+		}
+
+		return response.status(500).json({
+			stauts: "error",
+			message: `Internal Server Error: ${err.message}`,
+		});
+	}
+);
 
 app.listen(PORT, () => {
 	console.log(`Server running at http://localhost:${PORT}`);
